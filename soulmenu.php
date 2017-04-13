8145db7d22b8747f3b9fa19a0a411e2a5f2b7ad9
Arūnas Liuiza <ask@arunas.co>
2017-04-13 16:08:28 +0300

    create shortcode generator

--- soulmenu.php
+++ soulmenu.php
@@ -18,7 +18,74 @@ class SoulMenu {
     add_shortcode( 'soulmenu',            array( 'SoulMenu', 'shortcode' ) );
     add_action( 'wp_enqueue_scripts', 		array( 'SoulMenu', 'styles' ) );
 
+    add_action( 'admin_enqueue_scripts',  array( 'SoulMenu', 'admin_scripts' ) );
+    // TO DO: do a better Beaver Builder detection.
+		if ( isset( $_REQUEST['fl_builder'] ) && is_user_logged_in() ) {
+			add_action( 'wp_enqueue_scripts',  array( 'SoulMenu', 'admin_scripts' ) );
+		}
+    // Abnormally large priority is a workaround Beaver Builder trying to disable third party buttons in WP Editor.
+    add_filter( 'mce_external_plugins', array( 'SoulMenu', 'editor_button_js' ), 999999999 );
+		add_filter( 'mce_buttons', 			    array( 'SoulMenu', 'editor_button' ), 999999999 );
+
   }
+  /**
+	 * Add SoulMenu button to WP Editor
+	 *
+	 * @param array $buttons list of editor buttons.
+	 * @return array
+	 */
+	public static function editor_button( $buttons ) {
+		array_push( $buttons, 'soulmenu_shortcode' );
+		return $buttons;
+	}
+	/**
+	 * Add SoulMenu WP Editor button JS file
+	 *
+	 * @param array $plugin_array list of editor button JS files.
+	 * @return array
+	 */
+	public static function editor_button_js( $plugin_array ) {
+		$plugin_array['soulmenu'] = plugins_url( 'soulmenu-button.js', __FILE__ );
+		return $plugin_array;
+	}
+
+  /**
+	 * Enqueue SoulMenu WP Admin scripts
+	 */
+	public static function admin_scripts() {
+		wp_register_script( 'soulmenu-admin', plugins_url( 'soulmenu-admin.js', __FILE__ ), array( 'jquery' ) );
+    $menus = get_terms( 'nav_menu' );
+    foreach($menus as $key => $menu){
+      $menus[ $key ] = array(
+        'text'  => $menu->name,
+        'value' => $menu->term_id,
+      );
+    }
+		$data = array(
+			'icon' => plugins_url( 'soulmenu-button.png', __FILE__ ),
+			'texts' => array(
+				'main_label' 			=> __( 'Main', 'soulmenu' ),
+				'advanced_label'	=> __( 'Advanced', 'soulmenu' ),
+				'menu_label' 		  => __( 'Menu', 'soulmenu' ),
+				'style_label' 		=> __( 'Style', 'soulmenu' ),
+				'style_options'   => array(
+					array(
+						'text'  => __( 'Vertical (default)', 'soulmenu' ),
+						'value' => '',
+					),
+					array(
+						'text'  => __( 'Horizontal', 'soulmenu' ),
+						'value' => 'horizontal',
+					),
+				),
+				'menu_options'          => $menus,
+				'add_dialog_title'  		=> __( 'Add SoulMenu', 'soulmenu' ),
+			),
+		);
+		wp_localize_script( 'soulmenu-admin', 'soulmenu', $data );
+		wp_enqueue_script( 'soulmenu-admin' );
+	}
+
   public static function shortcode( $args = array(), $content = '' ) {
     $defaults = array(
       'style' => 'vertical',
